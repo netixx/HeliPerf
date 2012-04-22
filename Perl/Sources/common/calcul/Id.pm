30869e4ed52181841ff82f9d9df759736e9983eb
Firstname Lastname <chaster_killer@hotmail.fr>
2012-04-22 13:49:18 +0200

    Ajout d'une méthode permettant de mettre à jour les profils
    Chaque profil contient la liste des ids d'optionnels. Il faut donc
    mettre à jour les profils quand les ids changent.

--- Perl/Sources/common/calcul/Id.pm
+++ Perl/Sources/common/calcul/Id.pm
@@ -1,16 +1,44 @@
 package calcul::Id;
 
+use constant ID_INIT => 0;
+
 sub auto_set_categories {
 	my $categories = shift;
-	my $id = 0;
+	my $id = ID_INIT;
 
 	foreach my $categorie (@$categories) {
 		foreach my $item (@{$categorie->get_items}) {
-			$item->set_id($id);
-			$id++;
+			$id = incr_item($item, $id);
+		}
+	}
+}
+
+sub auto_update_cat_profils {
+	my ($categories, $profils) = @_;
+	my @table = ();
+
+	my $id = ID_INIT;
+
+	foreach my $categorie (@$categories) {
+		foreach my $item (@{$categorie->get_items}) {
+			$table[$item->get_id] = $id;
+			$id = incr_item($item, $id);
 		}
 	}
 
+	foreach my $profil (@$profils) {
+		my @ids = ();
+		foreach my $id (@{$profil->get_ids}) {
+			push @ids, $table[$id] if defined($table[$id]);
+		}
+		$profil->set_ids(\@ids);
+	}
+}
+
+sub incr_item {
+	my ($item, $id) = @_;
+	$item->set_id($id);
+	return $id++;
 }
 
 1;
